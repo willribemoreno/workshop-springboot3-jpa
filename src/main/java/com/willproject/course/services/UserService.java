95dc70e21afe62897ad70d280b761a550ff7fe74
William Ribeiro Moreno <wmoreno@primeit.pt>
2023-04-24 15:04:48 -0400

    [feat] Service layer + component registration

---
+++ src/main/java/com/willproject/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.willproject.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.willproject.course.entities.User;
+import com.willproject.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		// returns the object User within obj
+		return obj.get();
+	}
+}
